8c6e933514665e57eca0ba9ae11aced899c8de5d
duowngng <duowng.ng@gmail.com>
2024-05-20 23:49:43 +0700

    add: home page

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <div className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-x text-black">
+                    &copy; 2024 My Store. All rights reserved.
+                </p>
+            </div>
+        </div>
+    );
+}
+ 
+export default Footer;
